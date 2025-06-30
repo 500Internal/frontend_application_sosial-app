b0311309f077f79a4862bd02a27c985694e081ee
YammD123 <siamqueen52@gmail.com>
2025-06-30 21:01:46 +0700

    feat: add toolips

--- app/components/navigations/mainNavbar.tsx
+++ app/components/navigations/mainNavbar.tsx
@@ -1,9 +1,16 @@
+import { House } from 'lucide-react'
 import React from 'react'
 
 export default function Navbar() {
+  const navMainItems = [
+    {
+      name: "Home",
+      href: "/home"
+    }
+  ]
   return (
     <nav>
-      
+<House className='' />
     </nav>
   )
 }
