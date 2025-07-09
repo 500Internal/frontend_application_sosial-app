2cb5cb194a15194b333219beb6d9ee09cda5ad84
YammD123 <siamqueen52@gmail.com>
2025-07-09 16:40:50 +0700

    feat(notification): integrate useGetSomeNotifications in MainSidebarRight and implement NotificationSomeListSidebar component

---
+++ app/features/notification/components/notificationSomeListSidebar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import type { NotificationType } from 'shared/types/notificationType'
+
+type Props = {
+  notifications: NotificationType[]
+}
+
+export default function NotificationSomeListSidebar({ notifications }: Props) {
+  return (
+    <div>
+
+    </div>
+  )
+}
--- shared/ui/layoutParts/sidebar/mainSidebarRight.tsx
+++ shared/ui/layoutParts/sidebar/mainSidebarRight.tsx
@@ -1,12 +1,25 @@
 import { IoNotifications } from "react-icons/io5";
+import NotificationSomeListSidebar from "~/features/notification/components/notificationSomeListSidebar";
+import { useGetSomeNotifications } from "~/features/notification/hooks/useGetSomeNotifications";
 
 export default function MainSidebarRight() {
+  const { data: someNotifications } = useGetSomeNotifications();
   return (
     <aside className="hidden lg:flex top-16 right-0 h-[calc(100vh-4rem)] z-40 overflow-y-auto fixed bg-[#1d232a] flex-col gap-4 w-60 py-4 px-3 border-l border-gray-800">
-      <h2 className="text-lg font-semibold">Notifikasi</h2>
-      <div className="flex items-center gap-2 bg-gray-800 p-2 rounded-md">
-        <IoNotifications className="text-xl text-gray-400" />
-        <span className="text-sm">Kamu punya 3 notifikasi baru</span>
+      <h2 className="text-lg text-white font-semibold">
+        Notifikasi ({someNotifications?.data.length})
+      </h2>
+      <div className="">
+        {someNotifications?.data.length === 0 ? (
+          <div className="flex items-center gap-2">
+            <IoNotifications className="text-white/60" />
+            <span className="text-white/60">Tidak ada notifikasi</span>
+          </div>
+        ) : (
+          <NotificationSomeListSidebar
+            notifications={someNotifications?.data ?? []}
+          />
+        )}
       </div>
       <div className="text-sm text-gray-400">Teman Online: 5</div>
     </aside>
